29f78d2e136c019dfd581a391ff6bd652208f30c
ericgpinto <ericgrandopinto@gmail.com>
2025-02-20 16:01:36 -0300

    add balance microservice

---
+++ fc-wallet-balance/src/main/java/com/ericpinto/fcwalletbalance/FcWalletBalanceApplication.java
@@ -0,0 +1,13 @@
+package com.ericpinto.fcwalletbalance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcWalletBalanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcWalletBalanceApplication.class, args);
+	}
+
+}
